5ad216eef41376e53960f2e1e4b818f9a8b055c9
xoxowo <1qq11212@gmail.com>
2022-12-02 12:08:35 +0900

    DI 방법 1 예제 추가

--- sp5-chap03/src/main/java/spring/MemberRegisterService.java
+++ sp5-chap03/src/main/java/spring/MemberRegisterService.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 public class MemberRegisterService {
 	private MemberDao memberDao;
-	
+	// 생성자를 통해 의존 객체를 주입 받음
 	public MemberRegisterService(MemberDao memberDao) {
 		this.memberDao = memberDao;
 	}
 	
-	public Long regist(RegisterRequest req) {
+	public Long regist(RegisterRequest req) { //주입 받은 의존 객체의 메서드를 사용
 		Member member = memberDao.selectByEmail(req.getEmail());
 		if (member != null) {
 			throw new DuplicateMemberException("dup email" + req.getEmail());
