137678b23705f309b9d2b59b0da64cb5186bd909
xoxowo <1qq11212@gmail.com>
2023-01-03 14:00:22 +0900

    @Autowired 애노테이션 의존 주입 예제 2-1

--- sp5-chap04/src/main/java/config/AppCtx.java
+++ sp5-chap04/src/main/java/config/AppCtx.java
@@ -23,16 +23,14 @@ public class AppCtx {
 
     @Bean
     public MemberRegisterService memberRegSvc() {
-        return new MemberRegisterService(memberDao());
+        return new MemberRegisterService();
     }
 
     @Bean
     public ChangePasswordService changePwdSvc() {
-        ChangePasswordService pwdSvc = new ChangePasswordService();
-//        pwdSvc.setMemberDao(memberDao());
-        return pwdSvc;
+        return new ChangePasswordService();
     }
-    // 추가
+
     @Bean
     public MemberPrinter memberPrinter() {
     	return new MemberPrinter();
@@ -40,17 +38,14 @@ public class AppCtx {
     
     @Bean
     public MemberListPrinter listPrinter() {
-    	return new MemberListPrinter(memberDao(), memberPrinter());
+    	return new MemberListPrinter();
     }
-    // DI 세터 메서드 방식 추가 기입
+
     @Bean
     public MemberInfoPrinter infoPrinter() {
-    	MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-//    	infoPrinter.setMemberDao(memberDao());
-//    	infoPrinter.setPrinter(memberPrinter());
-    	return infoPrinter;
+    	return new MemberInfoPrinter();
     }
-    // 기본 버전 타입값 추가 기입
+
     @Bean
     public VersionPrinter versionPrinter() {
     	VersionPrinter versionPrinter = new VersionPrinter();
@@ -58,5 +53,50 @@ public class AppCtx {
     	versionPrinter.setMinorVersion(0);
     	return versionPrinter;
     }
-    
 }
+
+// public class AppCtx {
+//     // @Bean 애노테이션은 해당 메서드가 생성한 객체를 스프링 빈이라고 설정한다.
+//     @Bean // 이 빈은 각각의 빈 객체를 생성 
+//     public MemberDao memberDao() {
+//         return new MemberDao();
+//     }
+
+//     @Bean
+//     public MemberRegisterService memberRegSvc() {
+//         return new MemberRegisterService(memberDao());
+//     }
+
+//     @Bean
+//     public ChangePasswordService changePwdSvc() {
+//         ChangePasswordService pwdSvc = new ChangePasswordService();
+// //        pwdSvc.setMemberDao(memberDao());
+//         return pwdSvc;
+//     }
+//     // 추가
+//     @Bean
+//     public MemberPrinter memberPrinter() {
+//     	return new MemberPrinter();
+//     }
+    
+//     @Bean
+//     public MemberListPrinter listPrinter() {
+//     	return new MemberListPrinter(memberDao(), memberPrinter());
+//     }
+//     // DI 세터 메서드 방식 추가 기입
+//     @Bean
+//     public MemberInfoPrinter infoPrinter() {
+//     	MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+// //    	infoPrinter.setMemberDao(memberDao());
+// //    	infoPrinter.setPrinter(memberPrinter());
+//     	return infoPrinter;
+//     }
+//     // 기본 버전 타입값 추가 기입
+//     @Bean
+//     public VersionPrinter versionPrinter() {
+//     	VersionPrinter versionPrinter = new VersionPrinter();
+//     	versionPrinter.setMajorVersion(5);
+//     	versionPrinter.setMinorVersion(0);
+//     	return versionPrinter;
+//     }
+// }
