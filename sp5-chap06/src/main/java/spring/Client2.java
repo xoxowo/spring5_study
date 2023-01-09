25629a1c63c169c0d3b497fff2bd48834b194251
xoxowo <1qq11212@gmail.com>
2023-01-09 14:34:04 +0900

    빈 라이프사이클 커스텀 메서드 예제 2 주석 추가

--- sp5-chap06/src/main/java/spring/Client2.java
+++ sp5-chap06/src/main/java/spring/Client2.java
@@ -19,3 +19,15 @@ public class Client2 {
         System.out.println("Client2.close() 실행");
     }
 }
+
+/*
+설정 파일에서 initMethod속성과 destroyMethod속성을 \
+사용할 메서드인 이름으로 지정해주어야한다.
+
+    @Bean(initMethod = "connect", destroyMethod= "close")
+    public Client2 client2() {
+        Client2 client = new Client2();
+        client.setHost("host");
+        return client;
+    }
+ */
