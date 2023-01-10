66ab56584f69a96d17e45d256d5b90b53f22f281
xoxowo <1qq11212@gmail.com>
2023-01-10 20:52:36 +0900

    around 예제 추가

--- sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
+++ sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
@@ -10,11 +10,13 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect // 스프링 AOP 적용할 클래스에 붙이기
 public class ExeTimeAspect {
-        // 공통 기능을 적용할 대상 설정 
+
         @Pointcut("execution(public * chap07..*(..))")
-        private void publicTarget() {}
-        // ()에 들어있는 의미는 해당 메서드에 정의한 Pointcut에 공통기능을 적용한다는 것을 의미
+        private void publicTarget() {} // 공통 기능을 적용할 대상 설정  
+        // chap07 패키지와 그 하위 패키지에 위치한 public 메서드를 적용할 대상으로 설정했다는 의미
+        
         @Around("publicTarget()")
+        // ()에 들어있는 의미는 해당 메서드에 정의한 Pointcut에 공통기능을 적용한다는 것을 의미
         public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
             long start = System.nanoTime();
             try {
@@ -24,8 +26,9 @@ public class ExeTimeAspect {
                 long finish = System.nanoTime();
                 Signature sig = joinPoint.getSignature();
                 System.out.printf("%s.%s(%s) 실행 시간 : %d ns\n",
-                joinPoint.getTarget().getClass().getSimpleName(),
+                joinPoint.getTarget().getClass().getSimpleName(), 
                     sig.getName(),Arrays.toString(joinPoint.getArgs()),(finish - start));
-            }
+                //getSignature, getTarget, getArgs 등 각 메서드는 호출한 메서드의 시그니처, 대상 객체, 인자 목록을 구하는데 사용된다
+                }
         }
 }
---
+++ sp5-chap07/src/main/java/config/AppCtx.java
@@ -0,0 +1,25 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import aspect.ExeTimeAspect;
+import chap07.Calculator;
+import chap07.RecCalculator;
+
+@Configuration
+// @Aspect 붙인 클래스를 공통 기능으로 적용하기위해 붙이는 애노테이션
+@EnableAspectJAutoProxy 
+public class AppCtx {
+    
+    @Bean
+    public ExeTimeAspect exeTimeAspect() {
+        return new ExeTimeAspect();
+    }
+
+    @Bean
+    public Calculator calculator() {
+        return new RecCalculator();
+    }
+}
