032bce55af3a8ceb961d429ff377b1b9c9c155c8
xoxowo <1qq11212@gmail.com>
2023-01-11 18:32:36 +0900

    db 연동 프로젝트 예제 생성-수정

--- sp5-chap08/src/main/java/spring/MemberDao.java
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -1,29 +1,20 @@
 package spring;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MemberDao {
 
-	private static long nextId = 0;
-
-	private Map<String, Member> map = new HashMap<>();
-
 	public Member selectByEmail(String email) {
-		return map.get(email);
+		return null;
 	}
 
 	public void insert(Member member) {
-		member.setId(++nextId);
-		map.put(member.getEmail(), member);
 	}
 
 	public void update(Member member) {
-		map.put(member.getEmail(), member);
 	}
 
 	public Collection<Member> selectAll() {
-		return map.values();
+		return null;
 	}
 }
