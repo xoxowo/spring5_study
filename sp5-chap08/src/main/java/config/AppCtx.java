bd108d0b9fb036c1d9f2e425207a5d5ef16b4a2b
xoxowo <1qq11212@gmail.com>
2023-01-13 15:42:41 +0900

    db 연결 예제 추가 1

---
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppCtx {
+	
+}
