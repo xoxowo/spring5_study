5340d975b801c45a6fac4745a70505aa27bb81ed
xoxowo <1qq11212@gmail.com>
2023-01-25 20:08:54 +0900

    chap 09 초기 셋팅 생성

--- sp5-chap08/src/main/java/main/MainForCPS.java
+++ sp5-chap08/src/main/java/main/MainForCPS.java
@@ -14,7 +14,7 @@ public class MainForCPS {
         ChangePasswordService cps = ctx.getBean("changePwdSvc", ChangePasswordService.class);
 
         try {
-            cps.changePassword("a@a.com","1234", "1111");
+            cps.changePassword("a@a.com","1111", "1234");
             System.out.println("암호를 변경했습니다.");
         } catch (MemberNotFoundException e) {
             System.out.println("회원 데이터가 존재하지 않습니다.");
