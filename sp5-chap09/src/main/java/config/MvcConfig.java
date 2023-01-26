523e38647b1d3e38cd655b725ce2b708ac15068a
xoxowo <1qq11212@gmail.com>
2023-01-26 15:00:57 +0900

    스프링 mvc 코드 구현 1

--- sp5-chap09/src/main/java/config/MvcConfig.java
+++ sp5-chap09/src/main/java/config/MvcConfig.java
@@ -7,15 +7,15 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+@EnableWebMvc // 스프링 MVC 설정 활성화하는 애노테이션 다양한 설정을 생성한다.
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Override
+    @Override // 
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
 
-    @Override
+    @Override //jsp를 이용해서 컨트롤러의 실행 결과를 보여주기 위한 설정 추가
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/.", ".jsp");
     }
