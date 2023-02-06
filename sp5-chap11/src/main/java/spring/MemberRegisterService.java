02c190c33bf4272d1e1e62b5fb80faa3e702c0ca
xoxowo <1qq11212@gmail.com>
2023-02-06 15:52:06 +0900

    컨트롤러 클래스 빈 등록

--- sp5-chap11/src/main/java/spring/MemberRegisterService.java
+++ sp5-chap11/src/main/java/spring/MemberRegisterService.java
@@ -9,14 +9,14 @@ public class MemberRegisterService {
 		this.memberDao = memberDao;
 	}
 	
-	public Long regist(RegisterRequest req) { //주입 받은 의존 객체의 메서드를 사용
-		Member member = memberDao.selectByEmail(req.getEmail());
-		if (member != null) {
-			throw new DuplicateMemberException("dup email" + req.getEmail());
-		}
-		Member newMember = new Member(
-				req.getEmail(),req.getPassword(), req.getName(), LocalDateTime.now());
-		memberDao.insert(newMember);
-		return newMember.getId();
-	}
+	// public Long regist(RegisterRequest req) { //주입 받은 의존 객체의 메서드를 사용
+	// 	Member member = memberDao.selectByEmail(req.getEmail());
+	// 	if (member != null) {
+	// 		throw new DuplicateMemberException("dup email" + req.getEmail());
+	// 	}
+	// 	Member newMember = new Member(
+	// 			req.getEmail(),req.getPassword(), req.getName(), LocalDateTime.now());
+	// 	memberDao.insert(newMember);
+	// 	return newMember.getId();
+	// }
 }
