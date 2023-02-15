384bc508f9546b335cb68b9adf7670a2713d431e
xoxowo <1qq11212@gmail.com>
2023-02-15 22:27:59 +0900

    <form:errors> 태그 설정

--- sp5-chap12/src/main/java/controller/RegisterRequestValidator.java
+++ sp5-chap12/src/main/java/controller/RegisterRequestValidator.java
@@ -10,41 +10,41 @@ import org.springframework.validation.Validator;
 import spring.RegisterRequest;
 
 public class RegisterRequestValidator implements Validator {
-    private static final String emailRegExp =
-            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
-                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    private Pattern pattern;
+	private static final String emailRegExp = 
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+			"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private Pattern pattern;
 
-    public RegisterRequestValidator() {
-        pattern = Pattern.compile(emailRegExp);
-        System.out.println("RegisterRequestValidator#new(): " + this);
-    }
+	public RegisterRequestValidator() {
+		pattern = Pattern.compile(emailRegExp);
+		System.out.println("RegisterRequestValidator#new(): " + this);
+	}
 
-    @Override
-    public boolean supports(Class<?> clazz) {
-        return RegisterRequest.class.isAssignableFrom(clazz);
-    }
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return RegisterRequest.class.isAssignableFrom(clazz);
+	}
 
-    @Override
-    public void validate(Object target, Errors errors) {
-        System.out.println("RegisterRequestValidator#validate(): " + this);
-        RegisterRequest regReq = (RegisterRequest) target;
-        if (regReq.getEmail() == null || regReq.getEmail().trim().isEmpty()) {
-            errors.rejectValue("email", "required");
-        } else {
-            Matcher matcher = pattern.matcher(regReq.getEmail());
-            if (!matcher.matches()) {
-                errors.rejectValue("email", "bad");
-            }
-        }
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
-        ValidationUtils.rejectIfEmpty(errors, "password", "required");
-        ValidationUtils.rejectIfEmpty(errors, "confirmPassword", "required");
-        if (!regReq.getPassword().isEmpty()) {
-            if (!regReq.isPasswordEqualToConfirmPassword()) {
-                errors.rejectValue("confirmPassword", "nomatch");
-            }
-        }
-    }
+	@Override
+	public void validate(Object target, Errors errors) {
+		System.out.println("RegisterRequestValidator#validate(): " + this);
+		RegisterRequest regReq = (RegisterRequest) target;
+		if (regReq.getEmail() == null || regReq.getEmail().trim().isEmpty()) {
+			errors.rejectValue("email", "required");
+		} else {
+			Matcher matcher = pattern.matcher(regReq.getEmail());
+			if (!matcher.matches()) {
+				errors.rejectValue("email", "bad");
+			}
+		}
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "required");
+		ValidationUtils.rejectIfEmpty(errors, "password", "required");
+		ValidationUtils.rejectIfEmpty(errors, "confirmPassword", "required");
+		if (!regReq.getPassword().isEmpty()) {
+			if (!regReq.isPasswordEqualToConfirmPassword()) {
+				errors.rejectValue("confirmPassword", "nomatch");
+			}
+		}
+	}
 
 }
