0240259ae175185e27a89e82b3523dc3e95600e8
xoxowo <1qq11212@gmail.com>
2023-02-21 14:06:02 +0900

    13chap 세팅

---
+++ sp5-chap13/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,33 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import controller.LoginController;
+import controller.RegisterController;
+import spring.MemberRegisterService;
+import spring.AuthService;
+
+@Configuration
+public class ControllerConfig {
+
+    @Autowired
+    private MemberRegisterService memberRegSvc;
+    @Autowired
+    private AuthService authService;
+
+    @Bean
+    public RegisterController registerController(){
+        RegisterController controller = new RegisterController();
+        controller.setMemberRegisterService(memberRegSvc);
+        return controller;
+    }
+    @Bean
+    public LoginController loginController() {
+    	LoginController controller = new LoginController();
+    	controller.setAuthService(authService);
+    	return controller;
+    }
+   
+}
